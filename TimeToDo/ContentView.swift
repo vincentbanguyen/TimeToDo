35238d2fecf9b95025bea0913fd29feba5cf95ef
Vincent Nguyen <vincentbanguyen13@gmail.com>
2026-02-28 01:47:09 -0600

    bootstrap

---
+++ TimeToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeToDo
+//
+//  Created by Vincent Nguyen on 2/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeToDo/TimeToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeToDoApp.swift
+//  TimeToDo
+//
+//  Created by Vincent Nguyen on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
